8044542a4ba85aafa81015afbfab302d72342737
Georg Rath <georg.rath@imba.oeaw.ac.at>
2015-12-14 18:19:20 +0100

    uninstall.sh: implemented shellcheck suggestions

--- uninstall.sh
+++ uninstall.sh
@@ -26,34 +26,33 @@ if [ -z "$installDir" ]; then
     echo "Please specify an installation directory! Exiting..."
     exit 1
 fi
+
 export QUEUE_PREFIX=gepetools
 
-ppns=( 1 2 4 6 8 12 16 )
-
-if [ -d $installDir -a -f $installDir/.gepetools.install ]; then
-    sudo rm -rf $installDir/
+if [[ -d "$installDir" && -f "$installDir/.gepetools.install" ]]; then
+    sudo rm -rf "$installDir"
 else
     echo "Specified installation directory, '$installDir', appears invalid! Bailing..."
     exit 1
 fi
 
 for queue in $(qconf -sql); do
-    for pe in $(qconf -spl | grep ${QUEUE_PREFIX}_${queue}); do
-        qconf -dattr queue pe_list $pe $queue
+    for pe in $(qconf -spl | grep "${QUEUE_PREFIX}_${queue}"); do
+        qconf -dattr queue pe_list "$pe" "$queue"
     done
 done
 
 for pe in $(qconf -spl | grep "${QUEUE_PREFIX}_"); do
-    qconf -dp $pe
+    qconf -dp "$pe"
 done
 
 echo "You should have root privs for this next part.  Hope you're in sudoers..."
 
 # Add complex values to queues
 for queue in $(qconf -sql); do
-    qconf -dattr queue complex_values pcpus=99999 $queue
-    qconf -dattr queue complex_values nodes=99999 $queue
-    qconf -dattr queue complex_values ppn=99999 $queue
+    qconf -dattr queue complex_values pcpus=99999 "$queue"
+    qconf -dattr queue complex_values nodes=99999 "$queue"
+    qconf -dattr queue complex_values ppn=99999 "$queue"
 done
 
 # Remove complex attributes
