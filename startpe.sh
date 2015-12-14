56f72228ccd7ad021b193e4a769df7f0ccec512f
Georg Rath <georg.rath@imba.oeaw.ac.at>
2015-12-14 17:50:03 +0100

    startpe.sh: shellcheck suggestions implemented

--- startpe.sh
+++ startpe.sh
@@ -10,64 +10,62 @@ unset LD_LIBRARY_PATH
 cd /tmp
 
 PeHostfile2MPICHMachineFile() {
-   cat $1 | while read line; do
-      host=`echo $line|cut -f1 -d" "|cut -f1 -d"."`
-      nslots=`echo $line|cut -f2 -d" "`
+   while read -r line; do
+      host=$(echo "$line" | cut -f1 -d" " | cut -f1 -d".")
+      nslots=$(echo "$line" | cut -f2 -d" ")
       i=1
-      while [ $i -le $nslots ]; do
-         echo $host
-         i=`expr $i + 1`
+      while [ $i -le "$nslots" ]; do
+         echo "$host"
+         i=$((i + 1))
       done
-   done
+   done < "$1"
 }
 
 PeHostfile2MPICH2MachineFile(){
-    cat $1 | while read line; do
-        host=`echo $line|cut -f1 -d" "|cut -f1 -d"."`
-        nslots=`echo $line|cut -f2 -d" "`
-        echo $host:$nslots
-    done
+    while read -r line; do
+        host=$(echo "$line" | cut -f1 -d" " | cut -f1 -d".")
+        nslots=$(echo "$line" | cut -f2 -d" ")
+        echo "$host:$nslots"
+    done < "$1"
 }
 
 PeHostfile2Ansys(){
     local machines
-    local_host=`hostname`
-    num_local_host=`grep $local_host $1 | awk '{print $2}'`
+    local_host=$(hostname)
+    num_local_host=$(grep "$local_host" "$1" | awk '{print $2}')
     machines="$local_host:$num_local_host"
 
-    for host in `awk '{ print $1 }' $1 | grep -v $local_host`; do
-        num_procs=`grep $host $1 | awk '{ print $2 }'`
+    for host in $(awk '{ print $1 }' "$1" | grep -v "$local_host"); do
+        num_procs=$(grep "$host" "$1" | awk '{ print $2 }')
         machines="$machines:$host:$num_procs"
     done
-    echo $machines
+    echo "$machines"
 }
 
 PeHostfile2Linda(){
     local machines
-    while read line; do
-        host=`echo $line|cut -f1 -d" "|cut -f1 -d"."`
-        nslots=`echo $line|cut -f2 -d" "`
+    while read -r line; do
+        host=$(echo "$line" | cut -f1 -d" " | cut -f1 -d".")
+        nslots=$(echo "$line" | cut -f2 -d" ")
         if [ -n "$machines" ]; then
             machines="$machines,$host:$nslots"
         else
             machines="$host:$nslots"
         fi
-    done < $1
-    echo $machines
+    done < "$1"
+    echo "$machines"
 }
 
 PeHostfile2LAMbootSchema(){
-    cat $1 | while read line; do
-        host=`echo $line|cut -f1 -d" "|cut -f1 -d"."`
-        nslots=`echo $line|cut -f2 -d" "`
+    while read -r line; do
+        host=$(echo "$line" | cut -f1 -d" " | cut -f1 -d".")
+        nslots=$(echo "$line" | cut -f2 -d" ")
         echo "$host cpu=$nslots"
-    done
+    done < "$1"
 }
 
-# parse options
-catch_rsh=1
+me=$(basename "$0")
 
-me=`basename $0`
 
 # test number of args
 if [ $# -ne 1 ]; then
@@ -79,21 +77,21 @@ fi
 pe_hostfile=$1
 
 # ensure pe_hostfile is readable
-if [ ! -r $pe_hostfile ]; then
+if [ ! -r "$pe_hostfile" ]; then
    echo "$me: can't read $pe_hostfile" >&2
    exit 1
 fi
 
 # create machine-files for MPIs
-PeHostfile2MPICHMachineFile $pe_hostfile >> $TMPDIR/machines.mpich
-PeHostfile2MPICHMachineFile $pe_hostfile >> $TMPDIR/machines.mvapich
-PeHostfile2MPICHMachineFile $pe_hostfile >> $TMPDIR/machines.mvapich2
-PeHostfile2MPICH2MachineFile $pe_hostfile >> $TMPDIR/machines.mpich2
-PeHostfile2MPICHMachineFile $pe_hostfile >> $TMPDIR/machines.hpmpi
-PeHostfile2MPICH2MachineFile $pe_hostfile >> $TMPDIR/machines.intelmpi
-PeHostfile2LAMbootSchema $pe_hostfile >> $TMPDIR/machines.lam
-PeHostfile2Linda $pe_hostfile >> $TMPDIR/machines.linda
-PeHostfile2Ansys $pe_hostfile >> $TMPDIR/machines.ansys
+PeHostfile2MPICHMachineFile "$pe_hostfile" >> "$TMPDIR/machines.mpich"
+PeHostfile2MPICHMachineFile "$pe_hostfile" >> "$TMPDIR/machines.mvapich"
+PeHostfile2MPICHMachineFile "$pe_hostfile" >> "$TMPDIR/machines.mvapich2"
+PeHostfile2MPICH2MachineFile "$pe_hostfile" >> "$TMPDIR/machines.mpich2"
+PeHostfile2MPICHMachineFile "$pe_hostfile" >> "$TMPDIR/machines.hpmpi"
+PeHostfile2MPICH2MachineFile "$pe_hostfile" >> "$TMPDIR/machines.intelmpi"
+PeHostfile2LAMbootSchema "$pe_hostfile" >> "$TMPDIR/machines.lam"
+PeHostfile2Linda "$pe_hostfile" >> "$TMPDIR/machines.linda"
+PeHostfile2Ansys "$pe_hostfile" >> "$TMPDIR/machines.ansys"
 
 # Make script wrapper for 'rsh' available in jobs tmp dir
 rsh_wrapper=%%INSTALL_DIR%%/rsh
@@ -104,7 +102,7 @@ if [ ! -x $rsh_wrapper ]; then
 fi
 
 rshcmd=rsh
-ln -s $rsh_wrapper $TMPDIR/$rshcmd
+ln -s $rsh_wrapper "$TMPDIR/$rshcmd"
 
 # signal success to caller
 exit 0
